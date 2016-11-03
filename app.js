c1acfd32fc38765edf109567f15d95c34a1b8888
yinxueyuan <838476901@qq.com>
2016-11-03 16:47:39 +0800

    webpack develope

--- app.js
+++ app.js
@@ -4,26 +4,83 @@ global.G = {
   C: null,
   R: null,
   M: null
-};
+}
 
-const koa = require('koa');
-const router = require('koa-router');
-const onerror = require('koa-onerror');
+const koa = require('koa')
+const router = require('koa-router')
+const onerror = require('koa-onerror')
+const Jade = require('koa-jade')
+const webpack = require('webpack')
+const os = require('os')
+const network = os.networkInterfaces()
+const path = require('path');
 // const webpack = require('webpack');
 
-const request = require("./utils/request");
+const request = require("./utils/request")
 
 
-G.C = require('./configs/config');
+const proList = ['114.215.158.62']
+const testList = ['121.42.87.86']
+
+let evn = process.env.NODE_ENV || 'develpoment'
+let root = path.resolve(__dirname, '.')
+
+//环境判断
+for (var key in network){
+  var ip = network[key];
+  for(var i = 0; i < ip.length; i++){
+    if(proList.indexOf(ip[i].address) > -1){
+      evn = 'production';
+    }else if(testList.indexOf(ip[i].address > -1)){
+      evn = 'test';
+    }
+  }
+}
+
+debugger
+G.C = require('./configs/config')[evn]
+G.C.evn = evn
+G.C.root = root
+
+const isDev = (G.C.evn == 'development')
+const localUri = 'http://127.0.0.1'
 
 
-const app = koa();
+const app = koa()
 
-const isDev = (G.C.evn == 'development');
-const localUri = 'http://127.0.0.1';
+const webpackConfig =require('./webpack-config')[ isDev ? 'dev' : 'build' ]
+const compiler = webpack(webpackConfig)
+
+const devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+  static: {
+    colors: true,
+    chunks: false
+  }
+})
+
+const hotMiddleware = require('webpack-hot-middleware')(compiler)
+
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
 
 //接口代理
 // request(G.C.apiProxy).middleWare(app);
+
+const jade = new Jade({
+  viewPath: __dirname + "/view",
+  debug: false,
+  pretty: false,
+  compileDebug: false,
+  locals: {
+    staticPath: G.C.staticPath
+  },
+  app: app
+})
 
 //错误处理
 onerror(app, {
@@ -42,13 +99,22 @@ onerror(app, {
     }
 })
 
+// app.use(devMiddleware)
+//
+// app.use(hotMiddleware)
+
 
 //路由
-app.use(require('./configs/routers')());
+app.use(require('./configs/routers')())
 
-// app.use(koajade.middleware({
-//   viewPath:
-// }))
+app.use(function*(next) {
+    yield next
+    if (404 !== this.status) return;
+    this.status = 404;
+    this.body = {
+        view: '404'
+    }
+})
 
 /**
  * 监听端口
@@ -59,4 +125,4 @@ module.exports = app.listen(G.C.port, (err) => {
     return
   }
   console.log('listening at ' + localUri + ':' + G.C.port)
-});
+})
--- configs/config.js
+++ configs/config.js
@@ -1,63 +1,24 @@
 "use strict";
-var evn = process.env.NODE_ENV || 'develpoment';
-const path = require('path');
-const os = require('os');
-const network = os.networkInterfaces();
-
-const proList = ['114.215.158.62'];
-const testList = ['121.42.87.86'];
-
-//环境判断
-for (var key in network){
-  var ip = network[key];
-  for(var i = 0; i < ip.length; i++){
-    if(proList.indexOf(ip[i].address) > -1){
-      evn = 'production';
-    }else if(testList.indexOf(ip[i].address > -1)){
-      evn = 'test';
-    }
-  }
-}
-
-var root = path.resolve(__dirname, '..');
 
 var Config = {
   'development': {
     port: 3000,
     staticPath: '/static',
     static2: '/dist',
-    host: 'http://local.yinxueyuan.com',
-    apiProxy: {
-      apiServer: 'http://app-server-test.yinxueyuan.com',
-      cson: path.join(root, 'configs/model.cson'),
-      mock: path.join(root, 'mock')
-    }
+    host: 'http://local.yinxueyuan.com'
   },
   'test': {
     port: 3000,
     staticPath: '/static',
     static2: '/dist',
-    host: 'http://test.yinxueyuan.com',
-    apiProxy: {
-      apiServer: 'http://app-server-test.yinxueyuan.com',
-      cson: path.join(root, 'configs/model.cson'),
-      mock: path.join(root, 'mock')
-    }
+    host: 'http://test.yinxueyuan.com'
   },
   'production': {
     port: 3000,
     staticPath: '/static',
     static2: '/dist',
-    host: 'http://www.yinxueyuan.com',
-    apiProxy: {
-      apiServer: 'http://app-server.yinxueyuan.com',
-      cson: path.join(root, 'configs/model.cson'),
-      mock: path.join(root, 'mock')
-    }
+    host: 'http://www.yinxueyuan.com'
   }
 };
 
-Config[evn].evn = evn;
-Config[evn].root = root;
-
-module.exports = Config[evn];
+module.exports = Config;
