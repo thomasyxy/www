1f73caa8ee989dfde371aedaa65bcc99b5825e61
yinxueyuan <wb-yxy230225@alibaba-inc.com>
2016-11-07 10:16:41 +0800

    webpack config react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-console.log(123)
+ReactDOM.render(
+  <App />,
+  document.body.appendChild(document.createElement('div'))
+)
