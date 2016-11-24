31d14d98de15abcf6556e38b949ffbd0ec1cfb8b
yinxueyuan <838476901@qq.com>
2016-11-24 17:13:33 +0800

    jsonp api

--- configs/routers.js
+++ configs/routers.js
@@ -10,8 +10,9 @@ module.exports = function(app){
   router.get('/', ctrl.home.index);
   router.get('/yixuan/blog/getTitle', ctrl.blog.getTitle);
 
-  //fie 测试插件列表数据
-  router.get('/mockPlug', ctrl.blog.mockPlug);
+
+  router.get('/vueapp/jsonp', ctrl.vueapp.jsonp);
+  router.get('/vueapp/normal', ctrl.vueapp.normal);
 
 
   return router.middleware();
---
+++ controller/vueapp.js
@@ -0,0 +1,22 @@
+"use strict"
+
+module.exports = {
+  jsonp: function *(next){
+    var data = {'name': 'jifeng', 'company': 'taobao'};
+    if (this.query && this.query.callback) {
+      //console.log('请求2:'+params.query);
+      var str =  this.query.callback[0] + '(' + JSON.stringify(data) + ')';//jsonp
+      this.body = str;
+    } else {
+      this.body = JSON.stringify(data);
+    }
+    yield next
+  },
+  normal: function *(next){
+    var data = 11;
+    this.body = {
+      a: data
+    };
+    yield next
+  }
+}
