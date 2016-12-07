f6e2086fea04c9baaa691b789ee116027356d5eb
yinxueyuan <838476901@qq.com>
2016-12-07 16:58:36 +0800

    util ajax local

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,21 @@
 import * as MainConstants from '../constants';
+import fetch from '../utils/ajax';
 
 
 export function loadInitPageData(){
   const API_NAME = MainConstants.INIT_PAGE_DATA;
   return (dispatch) => {
-    dispatch({
-      type: API_NAME
-    });
+    fetch({
+      api: API_NAME,
+      suc: res => {
+        dispatch({
+          type: API_NAME,
+          result: res.data
+        });
+      },
+      err: msg => {
+        console.error(msg);
+      }
+    })
   };
 }
--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -1,16 +1,35 @@
-import fetch from 'fetch';
+// import fetch from 'fetch';
+import reqwest from 'reqwest';
 
 import Utils from './index';
 
 export default function fetch(params){
-  if(!params.url || !API[params.url]){
+  if(!params.api || !API[params.api]){
     return
   }
 
   if(Utils.isLocal()){
-    params.api = API[params.url]['local'];
+    params.api = API[params.api]['local'];
     fetchMock(params);
   }else{
 
   }
+}
+
+function fetchMock(params){
+  reqwest({
+    url: params.api,
+    type: params.type || 'json',
+    method: params.method || 'get',
+    success: (res) => {
+      if(res.success === true){
+        params.suc && params.suc(res.data);
+      }else{
+        params.err && params.err(res.message || '接口调用出错');
+      }
+    },
+    error: () => {
+      params.err && params.err('接口调用异常');
+    }
+  });
 }
