fdab43d1c4deb50cfd99d7ebe271b82e1b74f62b
yinxueyuan <838476901@qq.com>
2017-01-17 11:20:22 +0800

    feat: 优化导航条渲染函数

--- src/pages/mods/components/top-bar/index.jsx
+++ src/pages/mods/components/top-bar/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { Navbar, Nav, NavItem, NavDropdown, PanelGroup, Panel, ListGroup, ListGroupItem, Clearfix } from 'react-bootstrap';
+import { Menu, RightMenu, MenuItem, SubMenu } from 'fit-menu'
 import {Button} from 'fit-button';
+
 import Utils from '../../../../utils';
 
 require('./index.scss');
@@ -17,80 +18,30 @@ export default class TopBar extends React.Component {
     };
     this._renderNavbar = this._renderNavbar.bind(this);
     this._renderDropList = this._renderDropList.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
 
   componentWillMount() {
   }
 
-  handleSelect(activeKey) {
-    this.setState({ activeKey });
-  }
-
-  handleClick() {
-    console.log('基本用法 点击')
-  }
-
   _renderNavbar(navData) {
     return (
-      <Navbar default collapseOnSelect>
-        <Button className="top-menu-btn" onClick={this.handleClick.bind(this)}>
-          &#xe622;
-        </Button>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <div className="top-line">
-              <a className="top-title" href="./" rel="noopener"></a>
-              <div className="top-cur-pos">首页</div>
-            </div>
-          </Navbar.Brand>
-          <Navbar.Collapse>
-            <Nav>
-              {
-                navData.list.length !== 0 ? navData.list.map((item, key) =>
-                  item.subList ? (
-                    <NavDropdown key={key} title={item.title} id={`nav-dropdown-${key}`}>
-                      {
-                        this._renderDropList(item, key)
-                      }
-                    </NavDropdown>
-                  ) : (
-                    <NavItem key={key} onClick={() => { Utils.open(item.url) }}>
-                      {item.title}
-                    </NavItem>
-                  )
-                ) : ''
-              }
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar.Header>
-        <div className="top-info">33333</div>
-      </Navbar>
+      <Menu>
+        <MenuItem brand>首页</MenuItem>
+        { navData.list && navData.list.length > 0 ? this._renderDropList(navData.list, 1) : '' }
+      </Menu>
     )
   }
 
-  _renderDropList(item, key){
-    return (
-      <Clearfix>
-        <PanelGroup activeKey={this.state.activeKey} onSelect={this.handleSelect} accordion>
-        {
-          item.subList.length !== 0 ? item.subList.map((subItem, subKey) =>
-            <Panel collapsible key={subKey} header={subItem.title} eventKey={subKey}>
-              <ListGroup fill onClick={ () => { this.handleClickItem(subItem.url) }}>
-                {
-                  subItem.typeList.length !== 0 ? subItem.typeList.map((terItem, terKey) =>
-                    <ListGroupItem key={terKey} onClick={ () => { Utils.open(terItem.url, true) }}>
-                      {terItem.title}
-                    </ListGroupItem>
-                  ) : ''
-                }
-              </ListGroup>
-            </Panel>
-          ) : ''
-        }
-        </PanelGroup>
-      </Clearfix>
+  _renderDropList(list, level){
+    return list.map((item, key) =>
+      item.list && item.list.length > 0 ?
+      <SubMenu className={`menu-l-${level}`} key={key} title={item.title}>
+        { this._renderDropList(item.list, level + 1) }
+      </SubMenu>
+     :
+      <MenuItem className={`item-l-${level}`} key={key} onClick={() => { Utils.open(item.url, true) }}>
+        { item.title }
+      </MenuItem>
     )
   }
 
