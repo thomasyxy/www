971acf8e3800096ed25d8ffa76ac964f9a09c881
yinxueyuan <wb-yxy230225@alibaba-inc.com>
2017-02-11 14:50:36 +0800

    feat: 添加login页

--- src/pages/login/mods/login-page.jsx
+++ src/pages/login/mods/login-page.jsx
@@ -3,31 +3,59 @@ import React, { PropTypes } from 'react';
 import assign from 'object-assign';
 
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 
 class LoginPage extends React.Component {
   constructor (props) {
     super(props);
     this.state = assign({}, props, {
-
+      expanded: false
     });
+
+    this.handleClickLogin = this.handleClickLogin.bind(this)
+    this.handleClickReset = this.handleClickReset.bind(this)
   }
   componentWillMount() {
   }
+
+
+
+  handleClickLogin(expanded) {
+    this.setState({expanded: expanded});
+  };
+
+  handleClickReset() {
+    this.setState({expanded: true});
+  };
+
+
   render() {
-    const {
-    } = this.state;
-    const style = {
-      height: 500,
-      width: 800,
-    };
 
     return (
-      <div className="login-container">
-          <div>
-            <Paper className="login" style={style} zDepth={2} rounded={false} />
+      <Paper className="login-container" zDepth={1}>
+        <div className="input-group">
+          <div className="input-name">
+            <TextField
+              className="custom-input"
+              hintText=""
+              floatingLabelText="UserName"
+            />
           </div>
-      </div>
+          <div className="input-psd">
+            <TextField
+              className="custom-input"
+              hintText=""
+              floatingLabelText="Password"
+              type="password"
+            />
+          </div>
+        </div>
+        <div className="btn-group">
+          <RaisedButton label="Login" primary={true} />
+        </div>
+      </Paper>
     );
   }
 }
