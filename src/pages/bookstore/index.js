330c94f973f6a685e37092eb5ba93209a66e73eb
yinxueyuan <838476901@qq.com>
2017-07-04 17:25:52 +0800

    feat: door import Mui

--- src/pages/bookstore/index.js
+++ src/pages/bookstore/index.js
@@ -2,12 +2,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Door from './mods/door';
 
 import './index.scss';
 
+injectTapEventPlugin();
+
 ReactDOM.render(
-  <Door />,
+  <MuiThemeProvider>
+    <Door />
+  </MuiThemeProvider>,
   document.getElementById('container')
 );
